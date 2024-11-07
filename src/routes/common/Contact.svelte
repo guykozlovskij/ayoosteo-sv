975b72df2e27f8baa6e40fc7fbc1723c39a069d7
Guy Kozlovskij <80322586+guykozlovskij@users.noreply.github.com>
2024-11-07 08:10:16 +0000

    add buttons and styling for follow and cnotact

--- src/routes/common/Contact.svelte
+++ src/routes/common/Contact.svelte
@@ -10,3 +10,46 @@
     </div>
   </div>
 </section>
+
+<style lang="scss">
+  @use "../../styles/base";
+
+  #contact-component {
+  width: 100%;
+  background-color: base.$ayoosteoblue;
+  height: 550px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-left: base.$padding-left;
+  padding-right: base.$padding-right;
+  h2 {
+    font-size: base.$intro-text;
+    font-weight: normal;
+    padding-bottom: base.$padding-bottom;
+    text-align: center;
+    color: rgb(255, 255, 255);
+  }
+  h1 {
+    color: rgb(255, 255, 255);
+    font-size: base.$title;
+    padding-bottom: base.$padding-bottom;
+  }
+  p {
+    color: rgb(255, 255, 255);
+    font-size: base.$text-regular;
+    max-width: 500px;
+    text-align: center;
+    padding-bottom: base.$padding-bottom;
+  }
+  #contact-details {
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
--- src/routes/common/Follow.svelte
+++ src/routes/common/Follow.svelte
@@ -13,8 +13,7 @@
     </a>
     <a aria-label="linked in logo"target="_blank" href="https://uk.linkedin.com/in/ayo-oluade" rel="noreferrer">
       <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
-        <path
-          d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
       </svg>
     </a>
   </div>
@@ -22,4 +21,38 @@
 
 <style lang="scss">
   @use "../../styles/base";
+
+  #follow {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 290px;
+  h1 {
+    font-size: base.$title;
+    margin-bottom: base.$padding-bottom;
+  }
+  #social-links {
+    display: flex;
+    flex-direction: row;
+    padding-bottom: base.$padding-bottom;
+    svg {
+      fill: base.$ayoosteogreen;
+      margin-left: 8px;
+      margin-right: 8px;
+    }
+    svg:hover {
+      fill: base.$ayoosteoblue;
+    }
+  }
+  p {
+    a {
+      text-decoration: none;
+      color: base.$ayoosteogreen;
+    }
+    a:hover {
+      color: base.$ayoosteoblue;
+    }
+  }
+}
 </style>
