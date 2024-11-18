ebdb2970334d35d2a3e0fd65eaf7810525a757e9
Guy Kozlovskij <80322586+guykozlovskij@users.noreply.github.com>
2024-11-18 18:40:04 +0000

    add title

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { sveltePreprocess } from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
+import { sveltePreprocess } from 'svelte-preprocess'; 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
